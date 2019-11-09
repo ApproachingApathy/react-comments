b69bd81bb407f28b5089bd155e7d36316c02d913
Ephriam Henderson <orik162@gmail.com>
2019-11-09 08:58:36 -0600

    Updates

---
+++ precache-manifest.ba6dd6e8d0d1f1ccd86630225215aaf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa2de488a4a2882ecdabe75030138d6f",
+    "url": "/react-comments/index.html"
+  },
+  {
+    "revision": "eaf48f86d15d948a5270",
+    "url": "/react-comments/static/css/main.58346c5a.chunk.css"
+  },
+  {
+    "revision": "b20faf1e0fe7cc0de906",
+    "url": "/react-comments/static/js/2.bf3b5613.chunk.js"
+  },
+  {
+    "revision": "eaf48f86d15d948a5270",
+    "url": "/react-comments/static/js/main.8d694fca.chunk.js"
+  },
+  {
+    "revision": "f6a16411807ff3ee50d7",
+    "url": "/react-comments/static/js/runtime-main.820901bf.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-comments/static/media/logo.25bf045c.svg"
+  }
+]);
